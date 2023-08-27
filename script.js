dc40b2c4d629a7aa47c271d3fa595ab1a4386276
jpcp0614 <jpcp0614@gmail.com>
2023-08-26 22:42:38 -0300

    :sparkles: feat: Requisito 08

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnEntrar = document.querySelector('#btn-entrar');
-const inputEmail = document.querySelector('#input-email');
-const inputPassword = document.querySelector('#input-password');
+const inputEmail = document.querySelector('#input-email-header');
+const inputPassword = document.querySelector('#input-password-header');
 
 const checkInput = (email, password) => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
